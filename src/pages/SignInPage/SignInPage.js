13e945d5743e8c46b18ef85996a272f52c957b57
Zlata Kotlova <z.kotlova@informika.ru>
2019-07-16 16:15:25 +0300

    Sign in/Sign up page blueprint

---
+++ src/pages/SignInPage/SignInPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignInPage.scss';
+
+const SignInPage = () => {
+    return (
+        <div className='signin-page'>
+            Sign in
+        </div>
+    );
+};
+
+export default SignInPage;
