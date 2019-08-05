ce43419777e322bdf88293a6177984177c83c76a
Zlata Kotlova <z.kotlova@informika.ru>
2019-08-05 11:15:54 +0300

    Fetch collections with redux-saga

--- src/redux/shop/shop-sagas.js
+++ src/redux/shop/shop-sagas.js
@@ -1,8 +1,17 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import { shopActionTypes } from './shop-types';
+import { convertCollectionsSnapshotToMap, firestore } from "../../firebase/firebase";
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from "./shop-actions";
 
 export function* fetchCollectionsAsync() {
-    yield console.log('---+---');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (err) {
+        yield put(fetchCollectionsFailure(err.message));
+    }
 }
 
 export function* fetchCollectionsStart() {
