5ed12944a0e377f056c54b1e02eff38dace1c76c
Zlata Kotlova <z.kotlova@informika.ru>
2019-08-15 12:14:30 +0300

    CartIconContainer with compose

--- src/components/CartIcon/CartIconContainer.js
+++ src/components/CartIcon/CartIconContainer.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Mutation, Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
+import { compose } from 'redux';
 import queries from "../../graphql/queries";
 import mutations from "../../graphql/mutations";
 import CartIcon from "./CartIcon";
 
-const CartIconContainer = () => {
-    return (
-        <Query query={queries.GET_ITEM_COUNT}>
-            {
-                ({ data: { itemCount } }) => (
-                    <Mutation mutation={mutations.TOGGLE_CART_HIDDEN}>
-                        {
-                            (toggleCartHidden) =>
-                                <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-                        }
-                    </Mutation>
-                )
-            }
-        </Query>
-    );
-
+const CartIconContainer = ({ data: { itemCount }, toggleCartHidden }) => {
+    return <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
 };
 
-export default CartIconContainer;
+export default compose(
+    graphql(queries.GET_ITEM_COUNT),
+    graphql(mutations.TOGGLE_CART_HIDDEN, { name: 'toggleCartHidden' })
+)(CartIconContainer);
